12a92f6a3c661397251c2c2d68318be084c12d95
daks-dev <mail@daks.dev>
2023-02-01 00:07:00 +0300

    +

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,7 @@
   {description} />
 
 <main itemprop="mainContentOfPage">
-  <header class="container mb-8">
+  <header class="container">
     <div class="grid lg:grid-cols-12 gap-8 w-full pt-8">
       <div class="mr-auto place-self-center lg:col-span-7 2xl:col-span-8">
         <h1
@@ -34,62 +34,60 @@
           Строительная экспертиза
         </h1>
         <p
-          class="font-light
+          class="mb-4 font-light
                  text-gray-500 text-sm xs:text-md lg:text-lg dark:text-gray-400 uppercase">
           один из лидеров на строительном рынке в области консалтинга, лабораторного сопровождения
           строительства, обследования зданий и сооружений
         </p>
+        <div class="xs:hidden flex justify-around">
+          <Button
+            href="tel://{telephone.replace(/[\s-()]/g, '')}"
+            class="drop-shadow-lg"
+            color="light"
+            size="xl">
+            <Icon
+              icon="ic:round-phone-in-talk"
+              class="w-16 h-16 text-cyan-600 dark:text-cyan-700" />
+          </Button>
+          <Button
+            href="mailto:{email}"
+            class="drop-shadow-lg"
+            color="light"
+            size="xl">
+            <Icon
+              icon="ic:round-mail-outline"
+              class="w-16 h-16 text-cyan-600 dark:text-cyan-700" />
+          </Button>
+        </div>
+        <div
+          class="hidden xs:flex flex-col gap-y-2
+                    text-slate-600 dark:text-slate-400">
+          <a
+            class="font-semibold text-3xl xl:text-4xl hover:text-sky-500
+                   transition-all duration-200 ease-in-out"
+            href="tel://{telephone.replace(/[\s-()]/g, '')}">
+            {telephone}
+          </a>
+          <a
+            rel="nofollow noreferrer"
+            class="text-lg xl:text-xl hover:text-sky-500
+                   transition-all duration-200 ease-in-out"
+            href={address.url}
+            target="_blank"
+            itemprop="url">
+            {@html address.addressLocality}, {@html address.streetAddress}
+          </a>
+        </div>
       </div>
       <div
         class="hidden lg:flex
-                  placeholders lg:col-span-5 2xl:col-span-4 lg:mt-0">
+               placeholders lg:col-span-5 2xl:col-span-4 lg:mt-0">
         <VideoPlaceholder
-          class="h-auto w-auto max-w-full aspect-video"
+          class="w-full h-auto max-w-full"
           size="48" />
       </div>
     </div>
   </header>
-
-  <div class="container mb-16">
-    <div class="xs:hidden flex justify-around">
-      <Button
-        href="tel://{telephone.replace(/[\s-()]/g, '')}"
-        size="xl">
-        <Icon
-          icon="ic:round-phone-in-talk"
-          class="w-16 h-16" />
-      </Button>
-      <Button
-        href="mailto:{email}"
-        size="xl">
-        <Icon
-          icon="ic:round-mail-outline"
-          class="w-16 h-16" />
-      </Button>
-    </div>
-    <div
-      class="hidden xs:flex flex-col gap-y-4
-                text-slate-600 dark:text-slate-400">
-      <a
-        class="font-semibold
-               text-2xl sm:text-4xl lg:text-3xl xl:text-4xl
-               hover:text-sky-500
-               transition-all duration-200 ease-in-out"
-        href="tel://{telephone.replace(/[\s-()]/g, '')}">
-        {telephone}
-      </a>
-      <a
-        rel="nofollow noreferrer"
-        class="text-lg md:text-xl lg:text-lg xl:text-xl
-               hover:text-sky-500
-               transition-all duration-200 ease-in-out"
-        href={address.url}
-        target="_blank"
-        itemprop="url">
-        {@html address.addressLocality}, {@html address.streetAddress}
-      </a>
-    </div>
-  </div>
 
   <div class="placeholders container lg:content">
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-16 mb-16 items-center">
